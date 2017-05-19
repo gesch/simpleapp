697305c626a6750dea9eae88b134347b39b03bd8
gesch <guido.esch@direkt-gruppe.de>
2017-05-20 00:35:20 +0200

    docker build

--- src/test/java/de/solutionsdirekt/SimpleAppApplicationTests.java
+++ src/test/java/de/solutionsdirekt/SimpleAppApplicationTests.java
@@ -5,6 +5,12 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.URL;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class SimpleAppApplicationTests {
@@ -13,6 +19,32 @@ public class SimpleAppApplicationTests {
 	public void contextLoads() {
 
 		System.out.println("doing some testing");
-	}
+
+        String url = "http://simpleapp:8080/hello";
+
+        URL obj = null;
+        try {
+            obj = new URL(url);
+
+        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
+
+        // optional default is GET
+        con.setRequestMethod("GET");
+
+        //add request header
+        con.setRequestProperty("User-Agent", "Mozilla/5.0");
+
+        int responseCode = con.getResponseCode();
+            System.out.println("\nSending 'GET' request to URL : " + url);
+            System.out.println("Response Code : " + responseCode);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (ProtocolException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 
 }
